7706c9776b1190936faabbb4ad8eba49e9ca346a
RajkumarManala <159491938+RajkumarManala1@users.noreply.github.com>
2024-11-16 19:53:39 -0600

    cloned website

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Raj Kumar Manala | Personal Portfolio",
+  description: "Raj Kumar Manala is a Data Engineer & Analyst with more than 3 years of Expericence",
 };
 
 export default function RootLayout({
