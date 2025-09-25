a2174fae059f2b9981023281592b741379f8d2ae
RajkumarManala <159491938+RajkumarManala1@users.noreply.github.com>
2025-09-25 11:30:45 -0500

    send email fixed

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,9 +5,11 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export const sendEmail = async (formData: FormData) => {
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
