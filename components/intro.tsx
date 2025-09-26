d93c203af99c346f82c4254870edb6904568387f
RajkumarManala <159491938+RajkumarManala1@users.noreply.github.com>
2025-09-25 20:58:22 -0500

    corrected linkedin link

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/raj-kumar-manala-ab1b78134/"
+          href="https://www.linkedin.com/in/raj-kumar-manala-%E2%98%81%EF%B8%8F-ab1b78134/"
           target="_blank"
         >
           <BsLinkedin />
