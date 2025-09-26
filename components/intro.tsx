1741e7c4dd696dbf1cfcb44c33baf7df45ba2faa
RajkumarManala <159491938+RajkumarManala1@users.noreply.github.com>
2025-09-25 21:56:44 -0500

    renamed to resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,11 +91,11 @@ export default function Intro() {
 
   <a
   className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-  href="/CV.pdf"
+  href="/Rajkumar_Manala_Resume.pdf"
   target="_blank"
   rel="noopener noreferrer"
 >
-  Download CV{" "}
+  Resume {" "}
   <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
 </a>
 
