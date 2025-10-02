47c57488cd1ce9fa549a4540a89b0f78fbf32d74
RajkumarManala <159491938+RajkumarManala1@users.noreply.github.com>
2025-10-02 09:50:35 -0500

    added google analystics for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,14 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";   // ✅ Import Script from next/script
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Raj Kumar Manala | Personal Portfolio",
-  description: "Raj Kumar Manala is a Data Engineer & Analyst with more than 5+ years of Expericence",
+  description:
+    "Raj Kumar Manala is a Data Engineer & Analyst with more than 5+ years of Expericence",
 };
 
 export default function RootLayout({
@@ -24,6 +26,20 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
+        {/* ✅ Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CD24S95HF1"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CD24S95HF1');
+          `}
+        </Script>
+
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
